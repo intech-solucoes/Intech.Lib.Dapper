014a63793adac70ef9d51d1d1db9bd6da32bb5dc
Rony Moura <ronymmoura@gmail.com>
2019-12-01 15:25:23 -0300

    Criando lib de erros

---
+++ Erros/ErrosBancoItens.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Intech.Lib.Dapper.Erros
+{
+    public class ErrosBancoItens
+    {
+        public string MensagemBase { get; set; }
+        public List<KeyValuePair<string, string>> Itens { get; set; }
+    }
+}
